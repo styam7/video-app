674a1814d94aaa16bbf02a44e3cbfbd9cabc7456
satyam <styam.7@gmail.com>
2022-09-08 17:08:25 +0530

    Menu bar logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import styled from "styled-components"
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+    flex: 7;
+`;
+
+const Wrapper = styled.div``;
 
 const App = () => {
 
     return (
-        <>
-            appp
-        </>
+        <Container>
+            <Menu />
+            <Main>
+                <Navbar />
+                <Wrapper>
+                    video cards
+                </Wrapper>
+            </Main>
+        </Container>
     )
 }
 
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+import ytLogo from '../img/logo.png'
+
+const Container = styled.div`
+   flex: 1;
+   background-color: #202020;
+   height: 100vh;
+   color: #fff;
+   font-size: 14px;
+`;
+
+const Wrapper = styled.div`
+   padding: 18px 26px;
+`;
+
+const Logo = styled.div`
+   display: flex;
+   align-items: center;
+   gap: 5px;
+   font-weight: bold;
+   margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+   height: 25px;
+`;
+
+const Item = styled.div`
+   display: flex;
+   align-items: center;
+   gap: 20px;
+   cursor: pointer;
+`;
+const Menu = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Logo>
+                    <Img src={ytLogo} />
+                    Youtube
+                </Logo>
+                <Item>
+                    Home
+                </Item>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Menu
