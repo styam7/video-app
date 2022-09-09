2bd902144dc5e10a488f887fc7f8e2b4be15575a
satyam <styam.7@gmail.com>
2022-09-09 23:23:30 +0530

    NAVBAR

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import styled from "styled-components"
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components"
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme";
 
 
 const Container = styled.div`
@@ -8,23 +10,89 @@ const Container = styled.div`
 `;
 
 const Main = styled.div`
-    flex: 7;
+    flex: 6;
+    background-color: ${({theme}) => theme.bg};
 `;
 
 const Wrapper = styled.div``;
 
 const App = () => {
 
+    const [darkMode, setDarkMode] = useState(true)
+
     return (
-        <Container>
-            <Menu />
-            <Main>
-                <Navbar />
-                <Wrapper>
-                    video cards
-                </Wrapper>
-            </Main>
-        </Container>
+
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+            <Container>
+                <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+                <Main>
+                    <Navbar />
+                    <Wrapper>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1><h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1><h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1>
+                        <h1>Test</h1><h1>Test</h1>
+
+                        <h1>Test</h1><h1>Test</h1>
+                        <h1>Test</h1>
+                    </Wrapper>
+                </Main>
+            </Container>
+        </ThemeProvider>
     )
 }
 
