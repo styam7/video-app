d28178c6ccef45344bc8fff825301ae61687d635
satyam <styam.7@gmail.com>
2022-09-23 20:03:19 +0530

    express server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from "express"
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRouter from './routes/users.js'
+
 
 const app = express();
+dotenv.config();
 
+const connect = () => {
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => console.log("DB connected"))
+        .catch((err) => { throw err })
+}
+
+app.use("/api/users", userRouter)
 
 app.listen("5000", () => {
+    connect()
     console.log("server connected...")
 })
